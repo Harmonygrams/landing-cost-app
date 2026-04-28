36b70908b63bc118ce3441797530b2d4c643c151
Harmony <88977052+Harmonygrams@users.noreply.github.com>
2026-04-28 09:41:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 const router = require('./routes/index')
 require('dotenv').config()
 require('./utils/connectDb')()
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5001
 const app = express() 
 //middleware 
 app.use(cors())
-app.use(router)
 app.use(express.json())
+app.use(router)
 app.listen(PORT, () => {
     console.log('server is listening to ', PORT)
 })
